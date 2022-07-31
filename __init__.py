d5ac3797edadc457cecfa04a247694b16a1786c1
Jamie Wong <jamie.lf.wong@gmail.com>
2022-07-30 22:13:50 -0700

    Fix problem where series of prompts would only import every second one, and add support for cloze questions

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 # import the main window object (mw) from aqt
+import re
 import time
 from typing import List
 from aqt import mw
@@ -8,7 +9,7 @@ from aqt.utils import showInfo, qconnect
 from aqt.qt import QAction
 from aqt import gui_hooks
 
-from .bear import Prompt, get_prompts_from_bear
+from .bear import Prompt, get_cloze_from_bear, get_prompts_from_bear
 
 from anki.notes import Note
 from anki.decks import DeckId
@@ -19,36 +20,70 @@ from anki.models import NotetypeId
 
 # TODO: Move this into config
 deck_id = DeckId(1)
-note_type_id = NotetypeId(1654371237076)
+
+# Note type for "Bear Import Basic"
+basic_note_type_id = NotetypeId(1654371237076)
+
+# Note type for "Bear Import Cloze"
+cloze_note_type_id = NotetypeId(1659241489874)
 
 
-def import_from_bear() -> List[Prompt]:
+def note_already_exists(text) -> bool:
     if not mw:
-        return []
+        return True
+
+    # find_notes takes an Anki search string, in which certain characters are
+    # given special meaning. This can mean, for example, searching for the exact
+    # contents of the front of the card may not find the card.
+    #
+    # To counter-act this, we replace all of the special characters with spaces.
+    if not mw.col.find_notes(re.sub("[^a-zA-Z0-9-. ]", " ", text)):
+        return False
+    return True
+
+
+def get_new_prompts():
+    prompts_from_bear = get_prompts_from_bear()
+    new_prompts = []
+    for prompt in prompts_from_bear:
+        if not note_already_exists(prompt.question):
+            new_prompts.append(prompt)
+    return new_prompts
+
+
+def get_new_cloze():
+    cloze_from_bear = get_cloze_from_bear()
+    new_cloze = []
+    for cloze in cloze_from_bear:
+        if not note_already_exists(cloze.text):
+            new_cloze.append(cloze)
+    return new_cloze
+
+
+def import_from_bear():
+    if not mw:
+        return
 
     before = time.process_time()
 
-    prompts_from_bear = get_prompts_from_bear()
-
-    new_prompts = []
-
-    for prompt in prompts_from_bear:
-        matching_notes = mw.col.find_notes(prompt.question)
-        if not matching_notes:
-            new_prompts.append(prompt)
-
+    new_prompts = get_new_prompts()
     for prompt in new_prompts:
-        note = Note(mw.col, note_type_id)
+        note = Note(mw.col, basic_note_type_id)
         note.fields = [prompt.question, prompt.answer, prompt.context]
+        mw.col.add_note(note, deck_id)
+
+    new_cloze = get_new_cloze()
+    for cloze in new_cloze:
+        note = Note(mw.col, cloze_note_type_id)
+        back_extra = ""
+        note.fields = [cloze.text, back_extra, cloze.context]
         mw.col.add_note(note, deck_id)
 
     elapsed = time.process_time() - before
 
     showInfo(
-        f"Imported {len(new_prompts)} new prompts from Bear in {elapsed:.1f}s. " +
-        f"Topics: {', '.join(set(p.context for p in new_prompts))}")
-
-    return new_prompts
+        f"Imported {len(new_prompts) + len(new_cloze)} new prompts from Bear in {elapsed:.1f}s. " +
+        f"Topics: {', '.join(set(p.context for p in new_prompts) | set(c.context for c in new_cloze))}")
 
 
 # create a new menu item, "test"
--- bear.py
+++ bear.py
@@ -1,3 +1,4 @@
+from inspect import getclosurevars
 import re
 import sqlite3
 from typing import List, NamedTuple
@@ -5,12 +6,21 @@ from typing import List, NamedTuple
 # TODO: Move this into config
 bear_db_path = "/Users/jlfwong/Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/Application Data/database.sqlite"
 
-qa_pattern = re.compile('\nQ:(.*?)\nA:(.*?)\n(?:\n|$|[^a-zA-Z0-9"])')
+qa_pattern = re.compile(
+    r'(?<=\n)Q: (.*?)\nA:(.*?)(?:\n)(?:\n|$|[^a-zA-Z0-9-. "])')
+
+cq_pattern = re.compile(
+    r'(?<=\n)CQ: (.*?)(?:\n\n|\n$|\n[^a-zA-Z0-9-. "])', re.DOTALL)
 
 
 class Prompt(NamedTuple):
     question: str
     answer: str
+    context: str
+
+
+class Cloze(NamedTuple):
+    text: str
     context: str
 
 
@@ -23,5 +33,64 @@ def get_prompts_from_bear() -> List[Prompt]:
     return prompts
 
 
+cloze_pattern = re.compile("{{")
+
+
+def replace_newlines(text):
+    return text.replace("\n", "<br/>")
+
+
+def get_cloze_from_bear() -> List[Cloze]:
+    con = sqlite3.connect(bear_db_path)
+    prompts = []
+    for (text, title) in con.execute("select ZTEXT, ZTITLE from ZSFNOTE"):
+        for (cloze) in cq_pattern.findall(text):
+            card_front_body = cloze.strip()
+
+            # Replace all instances of "{{" with "{{cN" where "N" increments, and starts at 1 after
+            parts = []
+            for (n, p) in enumerate(card_front_body.split("{{")):
+                if n > 0:
+                    parts.append("{{c" + str(n) + "::")
+                parts.append(p)
+
+            card_front_body = replace_newlines("".join(parts))
+
+            prompts.append(Cloze(card_front_body, title))
+    return prompts
+
+
+ex = """
+Q: How many lbs per kg?
+A: ~2.2
+
+Q: How much caffeine is in a cup of tea?
+A: 50mg
+
+Q: How much caffeine is in a cup of coffee?
+A: 100mg
+
+Q: What is Jeff Nippard's recommendation for volume caffeine intake pre-workout?
+A: 4mg/kg of body weight (this is 260mg at 145lbs)
+"""
+
+ex = """
+CQ: The ingredients of a White Russian are
+- {{2 parts vodka}}
+- {{1 part Kahlua}}
+- {{Splash of heavy cream}}
+
+CQ: The ingredients of an Elderflower spritz are
+- {{1.5 parts St-Germain}}
+- {{2 parts white wine}}
+- {{2 parts club soda}}
+
+CQ: The ingredients of a Moscow Mule are
+- {{2 parts vodka}}
+- {{1/2oz lime juice}}
+- {{3 parts ginger beer}}
+"""
+
 if __name__ == "__main__":
-    print(get_prompts_from_bear())
+    for c in get_cloze_from_bear():
+        print(c)
